9802b2a87adf5e866c6b2c3a403781ddab0903fd
yadoomi <yadoomisucks@gmail.com>
2025-05-27 11:44:25 +0300

    Added Custom Item: testlol

--- src/main/java/net/yadoomi/baby/Baby.java
+++ src/main/java/net/yadoomi/baby/Baby.java
@@ -2,6 +2,7 @@ package net.yadoomi.baby;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.yadoomi.baby.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class Baby implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/yadoomi/baby/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.yadoomi.baby.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.yadoomi.baby.Baby;
+
+public class ModItems {
+
+    public static final Item TEST = registerItem("testlol", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Baby.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Baby.LOGGER.info("Registering Mod Items for " + Baby.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(TEST);
+        });
+    }
+
+}
